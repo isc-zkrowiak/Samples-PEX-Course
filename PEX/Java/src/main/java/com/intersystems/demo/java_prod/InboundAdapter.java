2b0439e882c601035fe1ece9a6f0538fb22fe71f
Zack <zkrowiak@intersystems.com>
2020-02-07 10:12:33 -0500

    simplifying package structure

---
+++ PEX/Java/src/main/java/com/intersystems/demo/java_prod/InboundAdapter.java
@@ -0,0 +1,12 @@
+package com.intersystems.demo.java_prod;
+
+
+import com.intersystems.demo.java_prod.InboundService;
+
+public class InboundAdapter extends com.intersystems.enslib.pex.InboundAdapter {
+
+    public void OnTask() throws Exception {
+
+
+    }
+}
---
+++ PEX/Java/src/main/java/com/intersystems/demo/java_prod/InboundService.java
@@ -0,0 +1,8 @@
+package com.intersystems.demo.java_prod;
+
+
+public class InboundService extends com.intersystems.enslib.pex.BusinessService {
+
+
+}
+
